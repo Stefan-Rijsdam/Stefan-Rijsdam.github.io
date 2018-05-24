1898666a5e171b447c708868f3ba6ad88b8a91b4
stefanrijsdam <stefan.rijsdam@avivasolutions.nl>
2018-05-24 16:07:26 +0200

    Updated static files

--- 238096fbf54ce5990307da3c0e3a5e63.js
+++ 238096fbf54ce5990307da3c0e3a5e63.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({12:[function(require,module,exports) {
+})({8:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],8:[function(require,module,exports) {
+},{}],4:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,7 +132,13 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":12}],46:[function(require,module,exports) {
+},{"./bundle-url":8}],2:[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":4}],35:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -154,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51031' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52208' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -255,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[46])
-//# sourceMappingURL=/dist/c84ffcde37b1caeaa0e2767187d02227.map
+},{}]},{},[35])
+//# sourceMappingURL=/dist/238096fbf54ce5990307da3c0e3a5e63.map
